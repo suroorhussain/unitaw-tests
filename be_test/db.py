98c179f0ca40ffe8778b85ecc068ceb5de41b4a1
Suroor Hussain <surusuroor@gmail.com>
2026-02-11 16:32:39 +0400

    Move event data into the database

---
+++ be_test/db.py
@@ -0,0 +1,33 @@
+import datetime
+from typing import Annotated
+from fastapi import Depends
+from sqlmodel import SQLModel, Field, Session, select, create_engine
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+class EventBase(SQLModel):
+    title: str
+    description: str
+    location: str
+    featured: bool
+    image: str | None = None
+    date: datetime.date
+    price: float
+
+class Event(EventBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+class EventDetail(EventBase):
+    id: int
--- be_test/main.py
+++ be_test/main.py
@@ -1,4 +1,6 @@
+from sqlmodel import select
 import auth
+import db
 
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
@@ -17,26 +19,32 @@ EVENTS = [
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+    db.create_db_and_tables()
+
 @app.get("/")
 async def root():
     return {"message": "Hello world!"}
 
 @app.get("/events")
-async def get_events(limit: int|None = 10, offset: int|None = 0) -> list[EventResponse]:
-    return EVENTS[offset:offset+limit]
+async def get_events(session: db.SessionDep, limit: int|None = 10, offset: int|None = 0) -> list[db.EventDetail]:
+    events = session.exec(select(db.Event).offset(offset).limit(limit)).all()
+    return events
 
 @app.post("/events")
-async def create_event(event: Event, current_user: Annotated[User, Depends(auth.get_current_user)]) -> EventResponse:
-    event_data = event.model_dump()
-    event_data["id"] = len(EVENTS) + 1
-    EVENTS.append(event_data)
-    return event_data
+async def create_event(event: db.EventBase, current_user: Annotated[User, Depends(auth.get_current_user)], session: db.SessionDep) -> db.EventDetail:
+    db_event = db.Event.model_validate(event)
+    session.add(db_event)
+    session.commit()
+    session.refresh(db_event)
+    return db_event
 
 @app.get("/events/{event_id}")
-async def get_event(event_id: int) -> EventResponse:
-    for event in EVENTS:
-        if event["id"] == event_id:
-            break
+async def get_event(event_id: int, session: db.SessionDep) -> db.EventDetail:
+    event = session.get(db.Event, event_id)
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
     return event
 
 @app.post("/login")
